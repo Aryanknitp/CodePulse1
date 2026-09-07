b69fb975eb48943d914dc85ff8eccd1c3e121ddb
AryanKumar9470 <aryankrbjunitp@gmail.com>
2026-09-07 11:24:45 +0530

    Change some path in backend server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,11 @@ import { startScheduler } from "./jobs/scheduler.js";
 if (await connectDatabase()) {
   console.log("Data");
 }
+
+app.get('/', (req, res) => {
+  res.send('Backend is up and running smoothly!');
+});
+
 app.listen(env.port, () =>
   console.log(`[server] API listening on http://localhost:${env.port}`),
 );
